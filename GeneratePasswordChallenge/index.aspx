935abe347aa9c22142aaf16c4cdeb0b43d9621df
Moise Leonard Ion <44782220+moiseleo@users.noreply.github.com>
2022-06-11 16:17:32 +0300

    Add files via upload

---
+++ GeneratePasswordChallenge/index.aspx
@@ -0,0 +1,45 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication4.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div >
+            <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Generate a one-time password</h2>
+            
+             
+
+            <h4>User ID&nbsp  <asp:TextBox ID="TextBox1" runat="server" Height="33px" Width="306px" ></asp:TextBox></h4>
+            <h4 > DateTime&nbsp;<asp:Label ID="labeldt" runat="server" Font-Bold="True" Font-Italic="True"></asp:Label></h4> 
+            <h3>
+            <asp:Button ID="Button1" runat="server" Text="Generate" OnClick="Button_Click" Height="39px" Width="103px" BorderStyle="Solid" ForeColor="#0066FF"  /></h3>
+            <h3>Password generate is :<asp:Label ID="l1" runat="server" Font-Bold="True" ForeColor="#00CC00" />   </h3>
+            <p>
+            <asp:Label ID="LTimer" runat="server" Font-Bold="True" ForeColor="#CC0066"></asp:Label>
+            &nbsp;
+             </p> 
+            <asp:ScriptManager ID="ScriptManager" runat="server"> </asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel" runat="server" UpdateMode="Always">
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="Timer" EventName="Tick" />
+                </Triggers>
+                <ContentTemplate>
+                    <h3>Valid for&nbsp; 
+                    <asp:Label ID="DisplayTextMinutes" runat="server" Text="00" Font-Size="X-Large"></asp:Label>
+                    <span>:</span>
+                    <asp:Label ID="DisplayTextSeconds" runat="server" Text="00" Font-Size="X-Large"></asp:Label>
+                        &nbsp;seconds </h3>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <asp:Timer ID="Timer" runat="server" Interval="1000" OnTick="Timer_Tick" Enabled="False" >
+            </asp:Timer>
+           
+        </div>
+    </form> 
+</body>
+</html>
---
+++ GeneratePasswordChallenge/index.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Button_Click(object sender, EventArgs e)
+        {
+            Random rand1 = new Random();
+            int len = rand1.Next(6, 12);
+            const string ValidChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder result = new StringBuilder();
+            Random rand2 = new Random();
+            var btn = (Button)sender;
+
+            if (TextBox1.Text == "" || TextBox1.Text == "Enter User!!!")
+            {
+
+                TextBox1.Text = "Enter User!!!";
+            }
+            else
+            {
+                while (0 < len--)
+                {
+                    result.Append(ValidChar[rand2.Next(ValidChar.Length)]);
+                }
+                l1.Text = result.ToString();
+                this.LTimer.Text = "Password valid for 30 seconds ";
+
+                if (Session["InputTime"] == null)
+                {
+                    Session["InputTime"] = "";
+                }
+
+                var newTime = Session["InputTime"].ToString() + "30";
+
+                Session["InputTime"] = newTime;
+
+                var currentTimeMinutes = (newTime.Length > 2) ?
+                    int.Parse(newTime.Substring(0, newTime.Length - 2)) :
+                    0;
+
+                var currentTimeSeconds = (newTime.Length > 2) ?
+                    int.Parse(newTime.Substring(newTime.Length - 2)) :
+                int.Parse(newTime);
+
+                Session["MinutesLeft"] = currentTimeMinutes;
+                Session["SecondsLeft"] = currentTimeSeconds;
+
+                DisplayTextMinutes.Text = String.Format("{0:00}", currentTimeMinutes);
+                DisplayTextSeconds.Text = String.Format("{0:00}", currentTimeSeconds);
+            }
+
+
+            Timer.Enabled = true;
+            Session["InputTime"] = null;
+            Session["testing"] = "SomeValue";
+            Session["MinutesLeft"] = 0;
+            Session["SecondsLeft"] = 30;
+
+        }
+
+        protected void Timer_Tick(object sender, EventArgs e)
+        {
+            var currentTimeMinutes = int.Parse(Session["MinutesLeft"].ToString());
+            var currentTimeSeconds = int.Parse(Session["SecondsLeft"].ToString());
+
+            currentTimeSeconds--;
+            if (currentTimeSeconds == 0 && currentTimeMinutes == 0)
+            {
+                Timer.Enabled = false;
+            }
+            else if (currentTimeSeconds < 0)
+            {
+                currentTimeMinutes--;
+                currentTimeSeconds = 59;
+            }
+
+            DisplayTextMinutes.Text = String.Format("{0:00}", currentTimeMinutes);
+            DisplayTextSeconds.Text = String.Format("{0:00}", currentTimeSeconds);
+            Session["MinutesLeft"] = currentTimeMinutes;
+            Session["SecondsLeft"] = currentTimeSeconds;
+            l1.Text = "Expired password!!!";
+            this.LTimer.Text = "Password invalid after 30 seconds!!!";
+        }
+    }
+}
